4ff968810d759d8080569da551575679225cf8ef
Minh Duc Nguyen <22853830+ngmiduc@users.noreply.github.com>
2023-08-03 14:30:58 +0200

    add google maps example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,246 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core'
+import { IonicModule } from '@ionic/angular'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { GoogleMap, MapType } from '@capacitor/google-maps'
+
+const STYLES = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#757575',
+      },
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.country',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#bdbdbd',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.neighborhood',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#181818',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1b1b1b',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#2c2c2c',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#8a8a8a',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#373737',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#3c3c3c',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#4e4e4e',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#000000',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#3d3d3d',
+      },
+    ],
+  },
+]
 
 @Component({
   selector: 'app-home',
@@ -7,7 +248,51 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonicModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor() {}
+
+  private googleMap: any = null
+  private mapType: 'terrain' | 'hybrid' = 'terrain'
+
+  ngOnInit() {
+    const init = async (): Promise<void> => {
+      const apiKey = 'GOGLE_MAPS_API_KEY'
+
+      this.googleMap = await GoogleMap.create({
+        id: 'my-map', // Unique identifier for this map instance
+        element: document.getElementById('map') as HTMLElement, // reference to the capacitor-google-map element
+        apiKey: apiKey, // Your Google Maps API Key
+        config: {
+          tilt: 0,
+          panControl: false,
+          rotateControl: false,
+          scaleControl: false,
+          zoomControl: false,
+          clickableIcons: false,
+          mapTypeControl: false,
+          disableDefaultUI: false,
+          fullscreenControl: false,
+          streetViewControl: false,
+          center: {
+            // The initial position to be rendered by the map
+            lat: 33.6,
+            lng: -117.9,
+          },
+          zoom: 8, // The initial zoom level to be rendered by the map
+          styles: STYLES,
+        },
+      })
+    }
+
+    init()
+  }
+
+  changeMapType(type: 'terrain' | 'hybrid'): void {
+    this.mapType = type
+    this.googleMap.setMapType(
+      type === 'terrain' ? MapType.Hybrid : MapType.Terrain
+    )
+  }
 }
